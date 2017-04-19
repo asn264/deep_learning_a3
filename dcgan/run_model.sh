8cd27c61a01fafa78d25bfcb9506183862c5ddae
ass502 <ass502@nyu.edu>
2017-04-19 15:19:43 -0400

    model with 10 epochs

--- dcgan/run_model.sh
+++ dcgan/run_model.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-#SBATCH -o model_curves/base_more_memory.out        # STDOUT
+#SBATCH -o model_curves/base_10_epochs.out        # STDOUT
 #SBATCH --mail-type=ALL      # notifications for job done & fail
 #SBATCH --mail-user=ass502@nyu.edu # send-to address
-#SBATCH -t 0-7:00 
+#SBATCH -t 0-24:00 
 #SBATCH --mem=10GB
 
 module load pytorch/intel
 
 module load torchvision
 
-python main.py --niter=3
+python main.py --niter=10 --outf='output/base_10_epochs/'
