2871af7c0d7ed783d82f82b4fac25954d3b8ff72
ass502 <ass502@nyu.edu>
2017-04-18 21:10:25 -0400

    more memory run finished

--- dcgan/run_model.sh
+++ dcgan/run_model.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #
-#SBATCH -o model_curves/base.out        # STDOUT
+#SBATCH -o model_curves/base_more_memory.out        # STDOUT
 #SBATCH --mail-type=ALL      # notifications for job done & fail
 #SBATCH --mail-user=ass502@nyu.edu # send-to address
-#SBATCH -t 0-5:00 
+#SBATCH -t 0-7:00 
+#SBATCH --mem=10GB
 
 module load pytorch/intel
 
 module load torchvision
 
-python main.py --niter=5
+python main.py --niter=3
