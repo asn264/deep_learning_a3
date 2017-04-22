377ed1808ae4b6c326d43691aa3688eb3a4c71ef
Aditi Nair <asn264@nyu.edu>
2017-04-22 11:36:11 -0400

    stdout path

--- dcgan/run_model.sh
+++ dcgan/run_model.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 #
-#SBATCH -o model_curves/base_10_epochs.out        # STDOUT
+#SBATCH -o cifar/baseline/baseline_10_epochs.out        # STDOUT
 #SBATCH --mail-type=ALL      # notifications for job done & fail
 #SBATCH --mail-user=asn264@nyu.edu # send-to address
 #SBATCH -t 0-30:00 
 #SBATCH --mem=10GB
 
 module load pytorch/intel
-
 module load torchvision
 
 python main.py --niter=10 --dataset='cifar10' --outf='cifar/baseline/'
