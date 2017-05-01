957ba1a6640a213ce4e46d455a89c8880873bbb5
Aditi Nair <asn264@nyu.edu>
2017-05-01 13:51:56 -0400

    compare from seed

--- dcgan/loading_scripts/compare_from_seed.py
+++ dcgan/loading_scripts/compare_from_seed.py
@@ -1,5 +1,7 @@
-'''Given the same noise vector z, condition on 10 different classes and visualize the output.'''
-'''Generate a 5x10 grid of best images for conditional models. Does 5 images for each class (cifar10).'''
+'''
+Given the same noise vector z, condition on 10 different classes and show the output. 
+Also show vanilla generation on the same noise vector.
+'''
 
 from __future__ import print_function
 import argparse
@@ -19,7 +21,7 @@ from torch.autograd import Variable
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default='cifar10', help='cifar10 | lsun | imagenet | folder | lfw | mnist')
-parser.add_argument('--dataroot', default='../cifar10_data/', help='path to dataset')
+parser.add_argument('--dataroot', default='../../cifar10_data/', help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
@@ -31,8 +33,8 @@ parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, def
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
-parser.add_argument('--netG_conditional', default='', help="path to netG (to continue training)")
-parser.add_argument('--netG', default='', help="path to netG (to continue training)")
+parser.add_argument('--netG_cond', default='../cifar/conditional/netG_epoch_19.m', help="path to netG (to continue training)")
+parser.add_argument('--netG', default='../cifar/baseline_normalized/netG_epoch_19.m', help="path to netG (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
 parser.add_argument('--outf', default='output/', help='folder to output images and model checkpoints')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
@@ -99,6 +101,15 @@ if opt.dataset == 'mnist':
 	nc = 1
 else:
 	nc = 3
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
 
 '''ORIGINAL NET G'''
 class _netG(nn.Module):
@@ -176,13 +187,21 @@ netG_cond.load_state_dict(torch.load(opt.netG))
 
 netG = _netG(ngpu)
 netG.apply(weights_init)
-netG.load_state_dict(torch.load(opt.netG))
+netG.load_state_dict(torch.load(opt.netG_cond))
 
-#We will generate 11 images from one noise vector: 1 from vanilla model, 10 from conditional model (one image per class)
-fixed_noise = torch.FloatTensor(1, nz).normal_(0, 1) #used for generating images
-one_hot_labels = torch.LongTensor([i for i in range(10)]).resize_(10,1) 
-one_hots = torch.zeros(10,opt.n_classes).scatter_(1, one_hot_labels, 1) 
-fixed_noise_with_conditionals = torch.cat([fixed_noise,one_hots],1) '''THIS WONT WORK'''
+'''
+We will generate 11 images from one noise vector: 
+- 1 from vanilla model
+- 10 from conditional model (one image per class) 
+'''
+
+fixed_noise = torch.FloatTensor(1, nz).normal_(0, 1) #fixed z vector for all images
+
+fixed_noise_repeating = fixed_noise.repeat(opt.n_classes,1) #matrix with z on each row
+one_hot_labels = torch.LongTensor([i for i in range(opt.n_classes)]).resize_(opt.n_classes,1) #list of labels
+one_hots = torch.zeros(10,opt.n_classes).scatter_(1, one_hot_labels, 1) #matrix of corresponding one-hots
+fixed_noise_with_conditionals = torch.cat([fixed_noise_repeating,one_hots],1) #concat fixed_noise_repeating and one_hots
+
 fixed_noise_with_conditionals.resize_(10, nz+opt.n_classes,1,1)
 fixed_noise.resize(1,nz,1,1)
 
@@ -192,6 +211,7 @@ fixed_noise_with_conditionals = Variable(fixed_noise_with_conditionals)
 fake = netG(fixed_noise)
 fake_conditional = netG_cond(fixed_noise_with_conditionals)
 
-'''SAVE ALL IMAGES'''
-#vutils.save_image(fake.data, '%s/fake_samples.png' % (opt.outf, epoch), normalize=True)
+vutils.save_image(fake.data, opt.outf+'vanilla_fake.png', normalize=True)
+vutils.save_image(fake_conditional.data, opt.outf+'conditional_fake.png', normalize=True)
+
 
